dc6cd01cfd3c0788714af01c8772aa200d5a5bae
Meastro ZI <meastro15012004@gmail.com>
2022-06-16 16:47:28 +0530

    commiting after adding new file

---
+++ fibonachi.py
@@ -0,0 +1,15 @@
+def fibo():
+    x=0
+    y=1
+    
+    n=int(input("enter your number"))
+    print(0)
+    
+    num=0
+    
+    for i in range(1,n):
+        num=num+i
+        print(num," ")
+            
+fibo()
+        
